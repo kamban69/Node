143b295a25c7f5c64cb282011fd33cd9c8d901ed
Thiruekamban <kamban69@gmail.com>
2018-09-07 00:52:57 +0530

    with default modules

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,4 +1,11 @@
- function Day1 ()  {}
+const _ = require("lodash");
+
+function Day1 ()  {}
     Day1.prototype.GetAge = () =>   90;
+    Day1.prototype.Add = (n1, n2) => { 
+        if(!(_.isNumber(n1) && _.isNumber(n2))) 
+            return 0;
+         return n1+n2;
+        } 
 
 module.exports = Day1;
--- tests/day1-test.js
+++ tests/day1-test.js
@@ -3,10 +3,28 @@ var expect = chai.expect;
 var day1Code = require("./../Day1/day1.js")
 
 describe(
-    "day1_Func", function(){
+    "day1_Func get age", function(){
         it('getAges() should return 90' , function(){   
             day1code          =  new day1Code([]);
             expect(day1code.GetAge()).to.equal(90); 
         })
+    }
+);
+
+describe(
+    "day1_Func add", function(){
+        it('add() should add and return when 2 valid inputs are sent' , function(){   
+            day1code  =  new day1Code([]);
+            expect(day1code.Add(1, 5)).to.equal(6); 
+        });
+    }
+);
+
+describe(
+    "day1_Func add", function(){
+        it('add() should return 0 when 2 valid inputs are sent' , function(){   
+            day1code  =  new day1Code([]);
+            expect(day1code.Add('1', '5')).to.equal(0); 
+        });
     }
 );
