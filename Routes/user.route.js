7ac803f5ba8c69fe5cd8bebcd87a9ce29151b43a
Thiruekamban Poongkunran <ThiruekambanPoo@youforce.net>
2018-11-20 17:46:33 +0530

    adding JWT

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -3,7 +3,43 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('../Schema/User.model');
+router.post('/signin', function(req, res){
+    User.findOne({email: req.body.email})
+    .exec()
+    .then(function(user) {
+       bcrypt.compare(req.body.password, user.password, function(err, result){
+          if(err) {
+             return res.status(401).json({
+                failed: 'Unauthorized Access'
+             });
+          }
+          const jwt = require('jsonwebtoken');
 
+if(result) {
+   const JWTToken = jwt.sign({
+        email: user.email,
+        _id: user._id
+      },
+      'secret',
+       {
+         expiresIn: '2h'
+       });
+       return res.status(200).json({
+         success: 'Welcome to the JWT Auth',
+         token: JWTToken
+       });
+  }
+          return res.status(401).json({
+             failed: 'Unauthorized Access'
+          });
+       });
+    })
+    .catch(error => {
+       res.status(500).json({
+          error: error
+       });
+    });;
+ });
 router.post('/signup', function(req, res) {
    bcrypt.hash(req.body.password, 10, function(err, hash){
       if(err) {
